63c2849b92270813a81f77d9b13affca73fb6ac3
Paul Zhutovsky <p.zhutovsky@amc.nl>
2016-11-03 11:05:38 +0100

    minor fixes, saving of embeddings and kl_div for reuse

--- optimize_tSNE.py
+++ optimize_tSNE.py
@@ -1,30 +1,28 @@
-import numpy as np
+import os.path as osp
+from itertools import product
+
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.manifold import TSNE
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
-from joblib import Parallel, delayed
-import os.path as osp
+import numpy as np
 import pandas as pd
-from itertools import product
+from joblib import Parallel, delayed
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from sklearn.preprocessing import StandardScaler
+
+from data_handling import apply_masking as mask_zeros
+
 
 FOLDER_DATA = '/data/shared/bvFTD/Machine_Learning/data'
 
 
 def load_data():
-    X = np.load(osp.join(FOLDER_DATA, 'data_set.npy'))
+    data = np.load(osp.join(FOLDER_DATA, 'data_set.npy'))
     group_labels = pd.read_csv(osp.join(FOLDER_DATA, 'class_labels.csv'))
     id_ftd = group_labels.ftd.as_matrix()
     id_neurol = group_labels.neurol.as_matrix()
     id_psych = group_labels.psych.as_matrix()
-    return X, id_ftd, id_neurol, id_psych
-
-
-def mask_zeros(X):
-    id_zeros = np.all(X == 0, axis=0)
-    X_masked = X[:, ~id_zeros]
-    return X_masked
+    return data, id_ftd, id_neurol, id_psych
 
 
 def make_scatter(data, id_ftd, id_neurol, id_psych):
@@ -43,39 +41,48 @@ def make_scatter(data, id_ftd, id_neurol, id_psych):
 
 def inner_loop_iteration(data, rate, perp, n, i, i_total):
     print '#Iteration: {} / {}'.format(i, i_total)
-    print '#Learning rate: ({}) #Perplexity: ({}) #Components: ({})'.format(rate, perp, n)
+    print 'Learning rate: ({}) Perplexity: ({}) #Components: ({})'.format(rate, perp, n)
     print
     tsne = TSNE(n_components=n, learning_rate=rate, perplexity=perp, init='pca', method='exact')
-    tsne_fit = tsne.fit(data)
-    return tsne_fit.kl_divergence_, tsne_fit.embedding_
+    tsne.fit(data)
+    return tsne.kl_divergence_, tsne.embedding_
 
 
 def run():
-    X, id_ftd, id_neurol, id_psych = load_data()
-    X = mask_zeros(X)
-    pca = PCA(n_components=500)
-    X_pca = pca.fit_transform(X)
+    print 'Load Data'
+    data, id_ftd, id_neurol, id_psych = load_data()
+    data = mask_zeros(data)
+    print 'Run PCA'
+    pca = PCA(n_components=50)
+    data_pca = pca.fit_transform(data)
     scaler = StandardScaler()
-    X_pca_scl = scaler.fit_transform(X_pca)
+    data_pca_scl = scaler.fit_transform(data_pca)
 
     learn_rate = np.linspace(100, 1000, 200)
     perplexity = np.arange(1, 51)
-    n_components = np.arange(2, 4)
+    n_components = [2, 3]
+
     i_total = len(learn_rate) * len(perplexity) * len(n_components)
-    n_jobs = 5
+    n_jobs = 15
 
     tsne_params = list(product(learn_rate, perplexity, n_components))
-    tsne_metrics = Parallel(n_jobs=n_jobs, verbose=1)(delayed(inner_loop_iteration)(X_pca_scl, rate, perp, n, i, i_total)
+    print 'Start Grid Search'
+    tsne_metrics = Parallel(n_jobs=n_jobs, verbose=1)(delayed(inner_loop_iteration)(data_pca_scl, rate, perp, n, i, i_total)
                                                       for i, (rate, perp, n) in enumerate(tsne_params))
 
     kl_div, embeddings = zip(*tsne_metrics)
-    kl_div_min, kl_div_idx = np.array(kl_div).min(), np.array(kl_div).argmin()
+    kl_div = np.array(kl_div)
+    # to make the embeddings better saveable we add a column of nans to the 2 dimensional embeddings (as a dummy)
+    # the final embeddings array has the shape: len(learn_rate) * len(perplexity) * len(n_components) x 67 x 3
+    embeddings = np.array([np.column_stack((e, np.ones(e.shape[0]) * np.nan)) if e.shape[1] == 2 else e
+                           for e in embeddings])
+    kl_div_min, kl_div_idx = kl_div.min(), kl_div.argmin()
     print 'Minumum KL-divergence found: {}, using the following parameters:'.format(kl_div_min)
     print 'Learning rate: {}, Perplexity: {}, Number of components: {}'.format(tsne_params[kl_div_idx][0],
                                                                                tsne_params[kl_div_idx][1],
                                                                                tsne_params[kl_div_idx][2])
 
-    make_scatter(embeddings[kl_div_idx], id_ftd, id_neurol, id_psych)
+    np.savez_compressed('tsne_results.npz', kl_div=kl_div, embeddings=embeddings, tsne_params=np.array(tsne_params))
 
 
 if __name__ == '__main__':
