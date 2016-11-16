9b6077fdbd1a0a3796e75b26d0a5de7584923a37
Paul Zhutovsky <p.zhutovsky@amc.nl>
2016-11-16 14:04:46 +0100

    implemented the structural covariance by Yun et al, 2015

---
+++ structural_covariance.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+class StructuralCovariance(object):
+
+    def __init__(self):
+        self.mean_roi = np.nan
+        self.std_roi = np.nan
+
+    def fit(self, X, y=None):
+        X = self.standardize_subjects(X)
+        self.mean_roi = X.mean(axis=0, keepdims=True)
+        self.std_roi = X.std(axis=0, keepdims=True)
+
+    def transform(self, X, y=None):
+        n_subj, n_dims = X.shape
+        X = self.standardize_subjects(X)
+        X_standard = self.standardize_rois(X)
+        ids_triu = np.triu_indices(n_dims, k=1)
+        structural_covariance = np.zeros((n_subj, ids_triu[0].size))
+
+        for id_subj in xrange(n_subj):
+            subj_data = X_standard[id_subj]
+            distances = (subj_data[:, np.newaxis] - subj_data[np.newaxis, :])
+            structural_covariance[id_subj, :] = 1./np.exp(distances[ids_triu] ** 2)
+
+        return structural_covariance
+
+    def fit_transform(self, X, y=None):
+        self.fit(X, y=None)
+        return self.transform(X, y=None)
+
+    @staticmethod
+    def standardize_subjects(X):
+        return (X - X.mean(axis=1, keepdims=True))/X.std(axis=1, keepdims=True)
+
+    def standardize_rois(self, X):
+        return (X - self.mean_roi)/self.std_roi
