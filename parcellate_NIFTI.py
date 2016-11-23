4e02f7cb7aca585d20c9e5df8e408e115d4e8c40
Paul Zhutovsky <w.b.bruin@amc.uva.nl>
2016-11-23 09:34:37 +0100

    checked parcellation; now uses load_data from data_handling

--- parcellate_NIFTI.py
+++ parcellate_NIFTI.py
@@ -1,27 +1,24 @@
 import os.path as osp
 
-import nibabel as nib
 import numpy as np
 import pandas as pd
 
-scans_dir = '/data/shared/bvFTD/VBM/default_LOF5/data'
-atlas_dir = '/data/shared/bvFTD/Machine_Learning/data/parcellated_GM_data'
+from data_handling import load_data
 
 
-def load_data(data_path):
-    return nib.load(data_path).get_data().astype(np.float64)
+ATLAS_DIR = '/data/shared/bvFTD/Machine_Learning/data/parcellated_GM_data'
 
 
 def parcellate_nifti(files_to_load):
 
     # Create dictionaries for atlas labels
-    cort_labels = pd.read_csv(osp.join(atlas_dir, 'cortical_labels.csv')).code_roi.values
-    subcort_labels = pd.read_csv(osp.join(atlas_dir, 'subcortical_labels.csv')).code_roi.values
+    cort_labels = pd.read_csv(osp.join(ATLAS_DIR, 'cortical_labels.csv')).code_roi.values
+    subcort_labels = pd.read_csv(osp.join(ATLAS_DIR, 'subcortical_labels.csv')).code_roi.values
 
     n_cort_labels, n_subcort_labels = cort_labels.size, subcort_labels.size
 
-    atlas_cort = load_data(osp.join(atlas_dir, 'HarvardOxford-cort-resampled.nii.gz'))
-    atlas_subcort = load_data(osp.join(atlas_dir, 'HarvardOxford-subcort-resampled.nii.gz'))
+    atlas_cort = load_data(osp.join(ATLAS_DIR, 'HarvardOxford-cort-resampled.nii.gz'), keepdims=True)
+    atlas_subcort = load_data(osp.join(ATLAS_DIR, 'HarvardOxford-subcort-resampled.nii.gz'), keepdims=True)
 
     n_files = len(files_to_load)
 
@@ -34,8 +31,8 @@ def create_parcellated_data(atlas_cort, atlas_subcort, cort_labels, files_to_loa
     data = np.zeros((len(files_to_load), n_cort_labels + n_subcort_labels))
     print 'Starting NIFTI parcellation... \n'
     for i, file_path in enumerate(files_to_load):
-        print 'Running subject {} out of {} ... '.format(i, n_files)
-        subj_data = load_data(file_path)
+        print 'Running subject {} out of {} ... '.format(i + 1, n_files)
+        subj_data = load_data(file_path, keepdims=True)
 
         cort_parcellation = [subj_data[atlas_cort == roi_cort].mean() for roi_cort in cort_labels]
         subcort_parcellation = [subj_data[atlas_subcort == roi_subcort].mean() for roi_subcort in subcort_labels]
